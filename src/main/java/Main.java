c5b87026f56c878b04a1339b4e68fe86ce5546c8
bvanitem <blainevanitem2000@gmail.com>
2023-10-21 01:46:44 -0600

    Logic of the Calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,45 @@
 //Change to push
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args){
-    System.out.println("Testing Dockerfile");
+    String tab = "Output: ";
+    String userCommand = "";
+    String[] command;
+    int totalComputation = 0;
+    String textualComputation = "";
+
+    Calculator calculator = new Calculator(); // Set up the Calculator object that will be used
+    Scanner scanner = new Scanner(System.in); // Read input from the user
+
+    System.out.println("Welcome to the Calculator!");
+    while(!"exit".equals(userCommand)){
+      System.out.print("Input: ");
+      userCommand = scanner.nextLine();
+
+      // Logic of Calculator
+      command = userCommand.split(" ");
+      if ("add".equals(command[0])) {
+        totalComputation = calculator.add(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+      } else if ("subtract".equals(command[0])) {
+        totalComputation = calculator.subtract(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+      } else if ("multiply".equals(command[0])) {
+        totalComputation = calculator.multiply(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+      } else if ("binary".equals(command[0])) {
+        textualComputation = calculator.intToBinaryNumber(Integer.parseInt(command[1]));
+        totalComputation = 0;
+      } else if ("fib".equals(command[0])) {
+        totalComputation = calculator.fibonacciNumberFinder(Integer.parseInt(command[1]));
+      }
+      if (totalComputation == 0) {
+        System.out.printf("%s%s%n%n",tab,textualComputation);
+      }
+      else {
+        System.out.printf("%s%s%n%n",tab,totalComputation);
+      }
+    }
+    if(userCommand == "exit"){
+      scanner.close();
+    }
   }
 }
